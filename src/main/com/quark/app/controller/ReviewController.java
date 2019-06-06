6223e43052f7a03e5f98865bf1e8c9f13559d9e0
Kujira <1849897307@qq.com>
2019-06-06 14:36:04 +0800

    房产\学历认证接口

--- src/main/com/quark/app/controller/ReviewController.java
+++ src/main/com/quark/app/controller/ReviewController.java
@@ -9,6 +9,7 @@ import com.quark.app.logs.AppLog;
 import com.quark.common.AppData;
 import com.quark.common.config;
 import com.quark.interceptor.AppToken;
+import com.quark.model.extend.Audit;
 import com.quark.model.extend.Certification;
 import com.quark.model.extend.SuperstarPrice;
 import com.quark.utils.DateUtils;
@@ -40,7 +41,7 @@ public class ReviewController extends Controller implements Serializable{
 					setAttr("SweetsResponse", response2);
 					renderMultiJson("SweetsResponse");
 					return;
-				}	
+				}
 				boolean save = false;
 				String message = "";
 				UploadFile upload_cover = getFile("video_url", config.videos_path);
@@ -132,12 +133,99 @@ public class ReviewController extends Controller implements Serializable{
 		
 		//房产认证
 		public void saveHouse() {
-			
+			String token;
+			ResponseValues response2;
+			boolean save = false;
+			String message = "";
+			try {
+				token = getPara("token");
+				if (!AppToken.check(token, this)) {
+					response2 = new ResponseValues(this, Thread.currentThread().getStackTrace()[1].getMethodName());
+					// 登陆失败
+					response2.put("message", "请重新登陆");
+					response2.put("code", 405);
+					setAttr("ReviewResponse", response2);
+					renderMultiJson("ReviewResponse");
+					return;
+				}
+				//判断用户是否有审核申请记录
+				UploadFile HouseImg_Url = getFile("house_url",config.images_path);
+				String user_id = AppToken.getUserId(token, this);
+				Audit Audit = new Audit();
+				Audit.dao.find("select * from certification where user_id =" + user_id);
+				if(Audit != null) {
+					save  = Audit.dao.set("house_url", FileUtils.renameToFile(HouseImg_Url))
+							.set("house_reason", "").set("house_status",2).update();
+				}else {
+					save  = Audit.dao.set("house_url", FileUtils.renameToFile(HouseImg_Url))
+							.set("user_id",user_id).save();
+				}
+				ResponseValues responseValues = new ResponseValues(this, Thread.currentThread().getStackTrace()[1].getMethodName());
+				if(save) {
+					responseValues.put("status", 1);
+					message = "申请成功";
+				}else {
+					responseValues.put("status", 0);
+					message = "申请失败";
+				}
+				responseValues.put("message",message);
+				responseValues.put("code", 200);
+				setAttr("ReviewResponse", responseValues);
+				renderMultiJson("ReviewResponse");
+			}
+			 catch (Exception e) {
+				AppLog.error(e, getRequest());
+			}finally{
+				AppLog.info("", getRequest());
+			}
 		}
 		
 		//学历认证
 		public void saveEdu() {
-			
+			String token;
+			ResponseValues response2;
+			boolean save = false;
+			String message = "";
+			try {
+				token = getPara("token");
+				if (!AppToken.check(token, this)) {
+					response2 = new ResponseValues(this, Thread.currentThread().getStackTrace()[1].getMethodName());
+					// 登陆失败
+					response2.put("message", "请重新登陆");
+					response2.put("code", 405);
+					setAttr("ReviewResponse", response2);
+					renderMultiJson("ReviewResponse");
+					return;
+				}
+				UploadFile EduImg_Url = getFile("edu_url",config.images_path);
+				String user_id = AppToken.getUserId(token, this);
+				Audit Audit = new Audit();
+				Audit.dao.find("select * from certification where user_id =" + user_id);
+				if(Audit != null) {
+					save  = Audit.dao.set("edu_url", FileUtils.renameToFile(EduImg_Url))
+							.set("edu_reason", "").set("edu_status",2).update();
+				}else {
+					save  = Audit.dao.set("edu_url", FileUtils.renameToFile(EduImg_Url)).set("edu_reason", "")
+							.set("user_id",user_id).set("edu_status",2).save();
+				}
+				ResponseValues responseValues = new ResponseValues(this, Thread.currentThread().getStackTrace()[1].getMethodName());
+				if(save) {
+					responseValues.put("status", 1);
+					message = "申请成功";
+				}else {
+					responseValues.put("status", 0);
+					message = "申请失败";
+				}
+				responseValues.put("message",message);
+				responseValues.put("code", 200);
+				setAttr("ReviewResponse", responseValues);
+				renderMultiJson("ReviewResponse");
+			}
+			 catch (Exception e) {
+				AppLog.error(e, getRequest());
+			}finally{
+				AppLog.info("", getRequest());
+			}
 		}
 		
 		//汽车认证
