2831f244e053c2a0865f2f237c1bbf71acf59be2
chen <396768835@qq.com>
2019-06-06 09:17:39 +0800

    添加审核接口未实现

---
+++ src/main/com/quark/app/controller/ReviewController.java
@@ -0,0 +1,48 @@
+package com.quark.app.controller;
+
+import java.io.Serializable;
+
+import com.jfinal.core.Controller;
+
+/**
+ * 
+ * @author TOM
+ *
+ */
+public class ReviewController extends Controller implements Serializable{
+	
+		//汽车认证
+		public void saveCar() {
+			
+		}
+		
+				
+		//食品认证
+		public void saveVideo() {
+			
+		}
+		
+		
+		//身份证认证
+		public void saveIdCard() {
+			
+		}
+		
+		//房产认证
+		public void saveHouse() {
+			
+		}
+		
+		//学历认证
+		public void saveEdu() {
+			
+		}
+		
+		//汽车认证
+		public void saveHeart() {
+			
+		}		
+		
+		
+		
+}
